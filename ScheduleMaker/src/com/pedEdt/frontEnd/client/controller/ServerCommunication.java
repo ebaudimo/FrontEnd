8b3430eb3888f9405f39e494cafa320377c790f5
vruffet <vincent.ruffet@etu.u-bordeaux1.fr>
2013-03-12 20:02:41 +0100

    Ajout des communications avec le serveur (fonctions create/update/delete) et debut création des popups formulaire

---
+++ ScheduleMaker/src/com/pedEdt/frontEnd/client/controller/ServerCommunication.java
@@ -0,0 +1,293 @@
+package com.pedEdt.frontEnd.client.controller;
+
+import com.google.gwt.http.client.Request;
+import com.google.gwt.http.client.RequestBuilder;
+import com.google.gwt.http.client.RequestCallback;
+import com.google.gwt.http.client.RequestException;
+import com.google.gwt.http.client.Response;
+import com.google.gwt.user.client.Window;
+import com.pedEdt.frontEnd.client.model.Module;
+import com.pedEdt.frontEnd.client.model.Semester;
+import com.pedEdt.frontEnd.client.model.Teaching;
+import com.pedEdt.frontEnd.client.model.TeachingUnit;
+
+public final class ServerCommunication {
+
+	private static ServerCommunication instance = null;
+	private static RequestBuilder builder;
+	
+	private ServerCommunication() {
+		super();
+	}
+
+	public final static ServerCommunication getInstance() {
+		if (ServerCommunication.instance == null) {
+			synchronized(ServerCommunication.class) {
+				if (ServerCommunication.instance == null) {
+					ServerCommunication.instance = new ServerCommunication();
+				}
+			}
+		}
+		return ServerCommunication.instance;
+	}
+
+
+	
+	/* Semester */
+
+	public void createSemester(Semester semester) {
+		builder = new RequestBuilder(RequestBuilder.POST, "proxy.jsp?url=http://localhost:8080/rest/service/create/semester");
+		builder.setHeader("Content-type", "application/xml");
+		builder.setRequestData(Semester.toXML.toXml(semester));
+		try {
+			builder.sendRequest(null, new RequestCallback() {
+				public void onError(Request request, Throwable exception) {
+					Window.alert("Erreur lors de la creation du semestre.");
+				}
+
+				public void onResponseReceived(Request request, Response response) {
+					if (response.getStatusCode() == 200) {
+						Window.alert("Semestre cree");
+					}
+				}
+			});
+		} catch (RequestException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void updateSemester(Semester semester) {
+		builder = new RequestBuilder(RequestBuilder.POST, "proxy.jsp?url=http://localhost:8080/rest/service/update/semester");
+		builder.setHeader("Content-type", "application/xml");
+		builder.setRequestData(Semester.toXML.toXml(semester));
+		try {
+			builder.sendRequest(null, new RequestCallback() {
+				public void onError(Request request, Throwable exception) {
+					Window.alert("Erreur lors de la modification du semestre.");
+				}
+
+				public void onResponseReceived(Request request, Response response) {
+					if (response.getStatusCode() == 200) {
+						Window.alert("Semestre modifie");
+					}
+				}
+			});
+		} catch (RequestException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void deleteSemester(int id) {
+		builder = new RequestBuilder(RequestBuilder.GET, "proxy.jsp?url=http://localhost:8080/rest/service/delete/semester/" + id);
+		try {
+			builder.sendRequest(null, new RequestCallback() {
+				public void onError(Request request, Throwable exception) {
+					Window.alert("Erreur lors de la suppression du semestre.");
+				}
+
+				public void onResponseReceived(Request request, Response response) {
+					if (response.getStatusCode() == 200) {
+						Window.alert("Semestre supprime");
+					}
+				}
+			});
+		} catch (RequestException e) {
+			e.printStackTrace();
+		}
+	}
+
+
+	
+	/* TeachingUnit */
+
+	public void createTeachingUnit(TeachingUnit teachingUnit) {
+		builder = new RequestBuilder(RequestBuilder.POST, "proxy.jsp?url=http://localhost:8080/rest/service/create/teachingUnit");
+		builder.setHeader("Content-type", "application/xml");
+		builder.setRequestData(TeachingUnit.toXML.toXml(teachingUnit));
+		try {
+			builder.sendRequest(null, new RequestCallback() {
+				public void onError(Request request, Throwable exception) {
+					Window.alert("Erreur lors de la creation du TeachingUnit.");
+				}
+
+				public void onResponseReceived(Request request, Response response) {
+					if (response.getStatusCode() == 200) {
+						Window.alert("TeachingUnit cree");
+					}
+				}
+			});
+		} catch (RequestException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void updateTeachingUnit(TeachingUnit teachingUnit) {
+		builder = new RequestBuilder(RequestBuilder.POST, "proxy.jsp?url=http://localhost:8080/rest/service/update/teachingUnit");
+		builder.setHeader("Content-type", "application/xml");
+		builder.setRequestData(TeachingUnit.toXML.toXml(teachingUnit));
+		try {
+			builder.sendRequest(null, new RequestCallback() {
+				public void onError(Request request, Throwable exception) {
+					Window.alert("Erreur lors de la modification du TeachingUnit.");
+				}
+
+				public void onResponseReceived(Request request, Response response) {
+					if (response.getStatusCode() == 200) {
+						Window.alert("TeachingUnit modifie");
+					}
+				}
+			});
+		} catch (RequestException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void deleteTeachingUnit(int id) {
+		builder = new RequestBuilder(RequestBuilder.GET, "proxy.jsp?url=http://localhost:8080/rest/service/delete/teachingUnit/" + id);
+		try {
+			builder.sendRequest(null, new RequestCallback() {
+				public void onError(Request request, Throwable exception) {
+					Window.alert("Erreur lors de la suppression du TeachingUnit.");
+				}
+
+				public void onResponseReceived(Request request, Response response) {
+					if (response.getStatusCode() == 200) {
+						Window.alert("TeachingUnit supprime");
+					}
+				}
+			});
+		} catch (RequestException e) {
+			e.printStackTrace();
+		}
+	}
+
+
+	
+	/* Module */
+
+	public void createModule(Module module) {
+		builder = new RequestBuilder(RequestBuilder.POST, "proxy.jsp?url=http://localhost:8080/rest/service/create/module");
+		builder.setHeader("Content-type", "application/xml");
+		builder.setRequestData(Module.toXML.toXml(module));
+		try {
+			builder.sendRequest(null, new RequestCallback() {
+				public void onError(Request request, Throwable exception) {
+					Window.alert("Erreur lors de la creation du Module.");
+				}
+
+				public void onResponseReceived(Request request, Response response) {
+					if (response.getStatusCode() == 200) {
+						Window.alert("Module cree");
+					}
+				}
+			});
+		} catch (RequestException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void updateModule(Module module) {
+		builder = new RequestBuilder(RequestBuilder.POST, "proxy.jsp?url=http://localhost:8080/rest/service/update/module");
+		builder.setHeader("Content-type", "application/xml");
+		builder.setRequestData(Module.toXML.toXml(module));
+		try {
+			builder.sendRequest(null, new RequestCallback() {
+				public void onError(Request request, Throwable exception) {
+					Window.alert("Erreur lors de la modification du Module.");
+				}
+
+				public void onResponseReceived(Request request, Response response) {
+					if (response.getStatusCode() == 200) {
+						Window.alert("Module modifie");
+					}
+				}
+			});
+		} catch (RequestException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void deleteModule(int id) {
+		builder = new RequestBuilder(RequestBuilder.GET, "proxy.jsp?url=http://localhost:8080/rest/service/delete/module/" + id);
+		try {
+			builder.sendRequest(null, new RequestCallback() {
+				public void onError(Request request, Throwable exception) {
+					Window.alert("Erreur lors de la suppression du Module.");
+				}
+
+				public void onResponseReceived(Request request, Response response) {
+					if (response.getStatusCode() == 200) {
+						Window.alert("Module supprime");
+					}
+				}
+			});
+		} catch (RequestException e) {
+			e.printStackTrace();
+		}
+	}
+
+
+	
+	/* Teaching */
+
+	public void createTeaching(Teaching teaching) {
+		builder = new RequestBuilder(RequestBuilder.POST, "proxy.jsp?url=http://localhost:8080/rest/service/create/teaching");
+		builder.setHeader("Content-type", "application/xml");
+		builder.setRequestData(Teaching.toXML.toXml(teaching));
+		try {
+			builder.sendRequest(null, new RequestCallback() {
+				public void onError(Request request, Throwable exception) {
+					Window.alert("Erreur lors de la creation du Teaching.");
+				}
+
+				public void onResponseReceived(Request request, Response response) {
+					if (response.getStatusCode() == 200) {
+						Window.alert("Teaching cree");
+					}
+				}
+			});
+		} catch (RequestException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void updateTeaching(Teaching teaching) {
+		builder = new RequestBuilder(RequestBuilder.POST, "proxy.jsp?url=http://localhost:8080/rest/service/update/teaching");
+		builder.setHeader("Content-type", "application/xml");
+		builder.setRequestData(Teaching.toXML.toXml(teaching));
+		try {
+			builder.sendRequest(null, new RequestCallback() {
+				public void onError(Request request, Throwable exception) {
+					Window.alert("Erreur lors de la modification du Teaching.");
+				}
+
+				public void onResponseReceived(Request request, Response response) {
+					if (response.getStatusCode() == 200) {
+						Window.alert("Teaching modifie");
+					}
+				}
+			});
+		} catch (RequestException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void deleteTeaching(int id) {
+		builder = new RequestBuilder(RequestBuilder.GET, "proxy.jsp?url=http://localhost:8080/rest/service/delete/teaching/" + id);
+		try {
+			builder.sendRequest(null, new RequestCallback() {
+				public void onError(Request request, Throwable exception) {
+					Window.alert("Erreur lors de la suppression du Teaching.");
+				}
+
+				public void onResponseReceived(Request request, Response response) {
+					if (response.getStatusCode() == 200) {
+						Window.alert("Teaching supprime");
+					}
+				}
+			});
+		} catch (RequestException e) {
+			e.printStackTrace();
+		}
+	}
+}
---
+++ ScheduleMaker/src/com/pedEdt/frontEnd/client/view/Forms.java
@@ -0,0 +1,450 @@
+package com.pedEdt.frontEnd.client.view;
+
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.FormPanel;
+import com.google.gwt.user.client.ui.FormPanel.SubmitEvent;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.PopupPanel;
+import com.google.gwt.user.client.ui.TextBox;
+import com.google.gwt.user.client.ui.VerticalPanel;
+import com.pedEdt.frontEnd.client.model.Module;
+import com.pedEdt.frontEnd.client.model.Semester;
+import com.pedEdt.frontEnd.client.model.TeachingUnit;
+
+public class Forms {
+
+	public static PopupPanel popupCreateSemester() {
+
+		final PopupPanel popupPanel = new PopupPanel(false, true);
+
+		final FormPanel form = new FormPanel();
+
+		VerticalPanel holder = new VerticalPanel();
+		
+		holder.add(new Label("Creer semestre"));
+
+		//year
+		HorizontalPanel yearPanel = new HorizontalPanel();
+		yearPanel.add(new Label("Annee : "));
+		final TextBox year = new TextBox();
+		yearPanel.add(year);
+		holder.add(yearPanel);
+
+		//number
+		HorizontalPanel numberPanel = new HorizontalPanel();
+		numberPanel.add(new Label("Numero : "));
+		final TextBox number = new TextBox();
+		numberPanel.add(number);
+		holder.add(numberPanel);
+
+		//startDate
+		HorizontalPanel startDatePanel = new HorizontalPanel();
+		startDatePanel.add(new Label("Date de debut : "));
+		final TextBox startDate = new TextBox();
+		startDatePanel.add(startDate);
+		holder.add(startDatePanel);
+
+		//endDate
+		HorizontalPanel endDatePanel = new HorizontalPanel();
+		endDatePanel.add(new Label("Date de fin : "));
+		final TextBox endDate = new TextBox();
+		endDatePanel.add(endDate);
+		holder.add(endDatePanel);
+		
+		//submit
+		Button submitButton = new Button("Valider", new ClickHandler() {
+
+			@Override
+			public void onClick(ClickEvent event) {
+				form.submit();
+			}
+		});
+		
+		//cancel
+		Button cancelButton = new Button("Annuler", new ClickHandler() {
+			
+			@Override
+			public void onClick(ClickEvent event) {
+				popupPanel.hide();
+			}
+		});
+
+		HorizontalPanel buttonsPanel = new HorizontalPanel();
+		buttonsPanel.add(submitButton);
+		buttonsPanel.add(cancelButton);
+		holder.add(buttonsPanel);
+
+		form.add(holder);
+		form.addSubmitHandler(new FormPanel.SubmitHandler() {
+
+			@Override
+			public void onSubmit(SubmitEvent event) {
+				Semester semester = new Semester();
+				semester.setYear((year.getText().trim().equals("")) ? 0 : Integer.parseInt(year.getText().trim()));
+				semester.setNumber((number.getText().trim().equals("")) ? 0 : Integer.parseInt(number.getText().trim()));
+				semester.setStartDate((startDate.getText().trim().equals("")) ? 0 : Integer.parseInt(startDate.getText().trim()));
+				semester.setEndDate((endDate.getText().trim().equals("")) ? 0 : Integer. parseInt(endDate.getText().trim()));
+
+				Window.alert(Semester.toXML.toXml(semester));
+				//ServerCommunication.getInstance().createSemester(semester);
+				popupPanel.hide();
+			}
+		});
+
+		popupPanel.setWidget(form);
+		return popupPanel;
+	} // popupCreateSemester()
+
+	
+	
+	public static PopupPanel popupUpdateSemester(final Semester semester) {
+
+		final PopupPanel popupPanel = new PopupPanel(false, true);
+
+		final FormPanel form = new FormPanel();
+
+		VerticalPanel holder = new VerticalPanel();
+		
+		holder.add(new Label("Modifier semestre"));
+
+		//year
+		HorizontalPanel yearPanel = new HorizontalPanel();
+		yearPanel.add(new Label("Annee : "));
+		final TextBox year = new TextBox();
+		year.setText("" + semester.getYear());
+		yearPanel.add(year);
+		holder.add(yearPanel);
+
+		//number
+		HorizontalPanel numberPanel = new HorizontalPanel();
+		numberPanel.add(new Label("Numero : "));
+		final TextBox number = new TextBox();
+		number.setText("" + semester.getNumber());
+		numberPanel.add(number);
+		holder.add(numberPanel);
+
+		//startDate
+		HorizontalPanel startDatePanel = new HorizontalPanel();
+		startDatePanel.add(new Label("Date de debut : "));
+		final TextBox startDate = new TextBox();
+		startDate.setText("" + semester.getStartDate());
+		startDatePanel.add(startDate);
+		holder.add(startDatePanel);
+
+		//endDate
+		HorizontalPanel endDatePanel = new HorizontalPanel();
+		endDatePanel.add(new Label("Date de fin : "));
+		final TextBox endDate = new TextBox();
+		endDate.setText("" + semester.getEndDate());
+		endDatePanel.add(endDate);
+		holder.add(endDatePanel);
+
+		//submit
+		Button submitButton = new Button("Valider", new ClickHandler() {
+
+			@Override
+			public void onClick(ClickEvent event) {
+				form.submit();
+			}
+		});
+		
+		//cancel
+		Button cancelButton = new Button("Annuler", new ClickHandler() {
+			
+			@Override
+			public void onClick(ClickEvent event) {
+				popupPanel.hide();
+			}
+		});
+
+		HorizontalPanel buttonsPanel = new HorizontalPanel();
+		buttonsPanel.add(submitButton);
+		buttonsPanel.add(cancelButton);
+		holder.add(buttonsPanel);
+
+		form.add(holder);
+		form.addSubmitHandler(new FormPanel.SubmitHandler() {
+
+			@Override
+			public void onSubmit(SubmitEvent event) {
+				semester.setYear((year.getText().trim().equals("")) ? 0 : Integer.parseInt(year.getText().trim()));
+				semester.setNumber((number.getText().trim().equals("")) ? 0 : Integer.parseInt(number.getText().trim()));
+				semester.setStartDate((startDate.getText().trim().equals("")) ? 0 : Integer.parseInt(startDate.getText().trim()));
+				semester.setEndDate((endDate.getText().trim().equals("")) ? 0 : Integer. parseInt(endDate.getText().trim()));
+
+				Window.alert(Semester.toXML.toXml(semester));
+				//ServerCommunication.getInstance().updateSemester(semester);
+				popupPanel.hide();
+			}
+		});
+
+		popupPanel.setWidget(form);
+		return popupPanel;
+	} // popupUpdateSemester(Semester semester)
+	
+	
+	
+	public static PopupPanel popupCreateTeachingUnit(final Semester semester) {
+
+		final PopupPanel popupPanel = new PopupPanel(false, true);
+
+		final FormPanel form = new FormPanel();
+
+		VerticalPanel holder = new VerticalPanel();
+		
+		holder.add(new Label("Creer UE"));
+
+		//code
+		HorizontalPanel codePanel = new HorizontalPanel();
+		codePanel.add(new Label("Code : "));
+		final TextBox code = new TextBox();
+		codePanel.add(code);
+		holder.add(codePanel);
+
+		//title
+		HorizontalPanel titlePanel = new HorizontalPanel();
+		titlePanel.add(new Label("Titre : "));
+		final TextBox title = new TextBox();
+		titlePanel.add(title);
+		holder.add(titlePanel);
+
+		//semester
+		holder.add(new Label("Liste deroulante semestre ?"));
+
+		//submit
+		Button submitButton = new Button("Valider", new ClickHandler() {
+
+			@Override
+			public void onClick(ClickEvent event) {
+				form.submit();
+			}
+		});
+		
+		//cancel
+		Button cancelButton = new Button("Annuler", new ClickHandler() {
+			
+			@Override
+			public void onClick(ClickEvent event) {
+				popupPanel.hide();
+			}
+		});
+
+		HorizontalPanel buttonsPanel = new HorizontalPanel();
+		buttonsPanel.add(submitButton);
+		buttonsPanel.add(cancelButton);
+		holder.add(buttonsPanel);
+
+		form.add(holder);
+		form.addSubmitHandler(new FormPanel.SubmitHandler() {
+
+			@Override
+			public void onSubmit(SubmitEvent event) {
+				TeachingUnit teachingUnit = new TeachingUnit();
+				teachingUnit.setCode(code.getText().trim());
+				teachingUnit.setTitle(title.getText().trim());
+				teachingUnit.setSemester(semester);
+
+				Window.alert(TeachingUnit.toXML.toXml(teachingUnit));
+				//ServerCommunication.getInstance().createTeachingUnit(teachingUnit);
+				popupPanel.hide();
+			}
+		});
+
+		popupPanel.setWidget(form);
+		return popupPanel;
+	} // popupCreateTeachingUnit(Semester semester)
+	
+	
+	
+	public static PopupPanel popupUpdateTeachingUnit(final TeachingUnit teachingUnit) {
+		
+		final PopupPanel popupPanel = new PopupPanel(false, true);
+
+		final FormPanel form = new FormPanel();
+
+		VerticalPanel holder = new VerticalPanel();
+		
+		holder.add(new Label("Modifier UE"));
+
+		//code
+		HorizontalPanel codePanel = new HorizontalPanel();
+		codePanel.add(new Label("Code : "));
+		final TextBox code = new TextBox();
+		code.setText(teachingUnit.getCode());
+		codePanel.add(code);
+		holder.add(codePanel);
+
+		//title
+		HorizontalPanel titlePanel = new HorizontalPanel();
+		titlePanel.add(new Label("Titre : "));
+		final TextBox title = new TextBox();
+		title.setText(teachingUnit.getTitle());
+		titlePanel.add(title);
+		holder.add(titlePanel);
+
+		//semester
+		holder.add(new Label("Liste deroulante semestre ?"));
+
+		//submit
+		Button submitButton = new Button("Valider", new ClickHandler() {
+
+			@Override
+			public void onClick(ClickEvent event) {
+				form.submit();
+			}
+		});
+		holder.add(submitButton);
+
+		form.add(holder);
+		form.addSubmitHandler(new FormPanel.SubmitHandler() {
+
+			@Override
+			public void onSubmit(SubmitEvent event) {
+				teachingUnit.setCode(code.getText().trim());
+				teachingUnit.setTitle(title.getText().trim());
+
+				Window.alert(TeachingUnit.toXML.toXml(teachingUnit));
+				//ServerCommunication.getInstance().updateTeachingUnit(teachingUnit);
+				popupPanel.hide();
+			}
+		});
+
+		popupPanel.setWidget(form);
+		return popupPanel;
+	} // popupUpdateTeachingUnit(TeachintUnit teachingUnit)
+	
+	
+	
+	public static PopupPanel popupCreateModule(final TeachingUnit teachingUnit) {
+
+		final PopupPanel popupPanel = new PopupPanel(false, true);
+
+		final FormPanel form = new FormPanel();
+
+		VerticalPanel holder = new VerticalPanel();
+		
+		holder.add(new Label("Creer module"));
+
+		//code
+		HorizontalPanel codePanel = new HorizontalPanel();
+		codePanel.add(new Label("Code : "));
+		final TextBox code = new TextBox();
+		codePanel.add(code);
+		holder.add(codePanel);
+
+		//title
+		HorizontalPanel titlePanel = new HorizontalPanel();
+		titlePanel.add(new Label("Titre : "));
+		final TextBox title = new TextBox();
+		titlePanel.add(title);
+		holder.add(titlePanel);
+
+		//semester
+		holder.add(new Label("Liste deroulante UE ?"));
+
+		//submit
+		Button submitButton = new Button("Valider", new ClickHandler() {
+
+			@Override
+			public void onClick(ClickEvent event) {
+				form.submit();
+			}
+		});
+		
+		//cancel
+		Button cancelButton = new Button("Annuler", new ClickHandler() {
+			
+			@Override
+			public void onClick(ClickEvent event) {
+				popupPanel.hide();
+			}
+		});
+
+		HorizontalPanel buttonsPanel = new HorizontalPanel();
+		buttonsPanel.add(submitButton);
+		buttonsPanel.add(cancelButton);
+		holder.add(buttonsPanel);
+
+		form.add(holder);
+		form.addSubmitHandler(new FormPanel.SubmitHandler() {
+
+			@Override
+			public void onSubmit(SubmitEvent event) {
+				Module module = new Module();
+				module.setCode(code.getText().trim());
+				module.setTitle(title.getText().trim());
+				module.setTeachingUnit(teachingUnit);
+
+				Window.alert(Module.toXML.toXml(module));
+				//ServerCommunication.getInstance().createModule(module);
+				popupPanel.hide();
+			}
+		});
+
+		popupPanel.setWidget(form);
+		return popupPanel;
+	} // popupCreateModule(TeachingUnit teachingUnit)
+	
+	
+	
+	public static PopupPanel popupUpdateModule(final Module module) {
+		
+		final PopupPanel popupPanel = new PopupPanel(false, true);
+
+		final FormPanel form = new FormPanel();
+
+		VerticalPanel holder = new VerticalPanel();
+		
+		holder.add(new Label("Creer Module"));
+
+		//code
+		HorizontalPanel codePanel = new HorizontalPanel();
+		codePanel.add(new Label("Code : "));
+		final TextBox code = new TextBox();
+		code.setText(module.getCode());
+		codePanel.add(code);
+		holder.add(codePanel);
+
+		//title
+		HorizontalPanel titlePanel = new HorizontalPanel();
+		titlePanel.add(new Label("Titre : "));
+		final TextBox title = new TextBox();
+		title.setText(module.getTitle());
+		titlePanel.add(title);
+		holder.add(titlePanel);
+
+		//semester
+		holder.add(new Label("Liste deroulante UE ?"));
+
+		//submit
+		Button submitButton = new Button("Valider", new ClickHandler() {
+
+			@Override
+			public void onClick(ClickEvent event) {
+				form.submit();
+			}
+		});
+		holder.add(submitButton);
+
+		form.add(holder);
+		form.addSubmitHandler(new FormPanel.SubmitHandler() {
+
+			@Override
+			public void onSubmit(SubmitEvent event) {
+				module.setCode(code.getText().trim());
+				module.setTitle(title.getText().trim());
+
+				Window.alert(Module.toXML.toXml(module));
+				//ServerCommunication.getInstance().updateModule(module);
+				popupPanel.hide();
+			}
+		});
+
+		popupPanel.setWidget(form);
+		return popupPanel;
+	} // popupUpdateModule(Module module)
+}
